4ef125587230f3a73759cdec3f6b890347b0830a
riyasahni <sahnir@uchicago.edu>
2022-10-07 05:05:45 +0000

    warmup part 1 complete

--- scripts/drive_square.py
+++ scripts/drive_square.py
@@ -4,22 +4,23 @@ import rospy
 from geometry_msgs.msg import Twist
 
 class MoveRobotSquare:
-	def __init__(self):
+""" This node moves a turtlebot in a square pattern. """"
+	def __init__(self): # define node, publisher, and initiate toggle
 		self.counter = 1
 		rospy.init_node('drive_in_square')
 		self.publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 		self.move = Twist()
 	def run(self):
 		while not rospy.is_shutdown():
-			if self.counter%2 == 0: # turn now
+			if self.counter%2 == 0: # if toggle is even, turn now
 				self.move.angular.z = 0.55
 				self.move.linear.x = 0
-			else: # go straight
+			else: # if toggle is odd go straight
 				self.move.angular.z = 0
 				self.move.linear.x = 0.1
-			self.counter += 1
+			self.counter += 1 # update the toggle by incrementing by 1
 			self.publisher.publish(self.move)
-			rospy.sleep(3)
+			rospy.sleep(3) # allow robot to travel straight or to turn for 3 seconds before switching motion
 
 if __name__ == '__main__':
 	node = MoveRobotSquare()
