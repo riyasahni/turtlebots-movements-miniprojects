8790e651d8bbd9c25057999cc89d093be8a2125b
riyasahni <sahnir@uchicago.edu>
2022-10-14 16:29:14 +0000

    yay finally! just need to clean up bags and code

--- scripts/wall_follower.py
+++ scripts/wall_follower.py
@@ -38,60 +38,69 @@ class FollowWall(object):
 		# and 'left_dist' variables to
 
 		for a in range(0, 359):
-			if (a <= 20 or a>=339) and data.ranges[a] != 0:
+			if (a <= 10 or a>=349) and data.ranges[a] != 0:
 				min_front_dist=data.ranges[a]
-			if(a > 55 and a <= 125) and data.ranges[a] != 0:
+			if(a > 45 and a <= 135) and data.ranges[a] != 0:
 				left_dist = data.ranges[a]
 
 		# finding the distance to nearest object to the front of robot
 		# and saving the distance and angle it is at to robot
-		for a in range(0, 20):
+		for a in range(0, 10):
 			if data.ranges[a] <= min_front_dist and data.ranges[a] != 0:
 				min_front_dist = data.ranges[a]
 				min_front_angle = a
-		for a in range (339, 359):
+		for a in range (349, 359):
 			if data.ranges[a] <= min_front_dist and data.ranges[a] != 0:
 				min_front_dist = data.ranges[a]
 				min_front_angle = a
 
 		# finding distance to nearest object to the left of robot
 		# and saving nearest left distance and angle it is at to robot
-		for a in range(55, 135):
+		for a in range(35, 145):
 			if data.ranges[a] <= left_dist  and data.ranges[a] != 0:
 				left_dist = data.ranges[a]
 				left_angle = a
 
 		# default robot to just move forward if there is not left wall nearby or
 		# any wall coming up in front
-		self.twist.linear.x = 0.1
-		self.twist.angular.z = 0
-
-		# case 1: wall on LHS and you are too far from it
-		if left_dist  > 0.5 and left_dist != float('inf'):
+		if min_front_dist > 0.6:
+			print("min front dist", min_front_dist)
 			self.twist.linear.x = 0.1
-			self.twist.angular.z = 0.75 # turn towards wall to get closer
-		elif .35 <= left_dist <= 0.5:
+			self.twist.angular.z = 0
+		# case 1: wall on LHS and you are too far from it
+		if (left_dist  > 0.5 and left_dist != float('inf')): #and min_front_dist <=0.6):
+			print("left dist", left_dist)
+			print("turn closer to wall")
+			self.twist.linear.x = 0.05
+			self.twist.angular.z = 0.65 # turn towards wall to get closer
+		if .35 <= left_dist <= 0.55:
 			# adjust slightly into the wall to become more perpendicular
 			if left_angle > 95:
-				self.twist.linear.x = 0.1
-				self.twist.angular.z = 0.4
+				print("adjust into wall")
+				self.twist.linear.x = 0.08
+				self.twist.angular.z = 0.7
 			# else if too close, adjust slightly away from wall to become
 			# more perpendicular
 			elif left_angle < 85:
-				self.twist.linear.x = 0.1
-				self.twist.angular.z = -0.4
+				print("adjust away from wall")
+				self.twist.linear.x = 0.09
+				self.twist.angular.z = -0.7
 			# else if perpendicular to wall already, keep straight!
 			else:
+				print("keep parallel")
 				self.twist.linear.x = 0.1
 				self.twist.angular.z = 0
 		# case 2: wall on LHS and you are too close to it
 		elif left_dist < 0.25  and left_dist != 0:
+			print("turn away from wall")
 			# turn away from wall to maintain distance b/w wall & robot
 			self.twist.linear.x = 0.1
-			self.twist.angular.z = -0.75
+			self.twist.angular.z = -0.55
 
 		# case 3: arrived front of wall and robot needs to turn some way
 		if min_front_dist <= 0.6 and min_front_dist!=0:
+			print("min front dist 90", min_front_dist)
+			print("turn 90 deg")
 			self.twist.linear.x = 0
 			self.twist.angular.z = -0.65 # choosing to make robot turn right
 
